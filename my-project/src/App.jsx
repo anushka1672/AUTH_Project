c931f55f362c34cd0d7da402fe0c20bdba98a97f
Anushka_27 <anushkakhewadia@gmail.com>
2026-09-21 16:02:50 +0530

    feat:signUp ui with protected dashboard

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Dashboard } from "./components/Dashboard";
 import { Login } from "./components/Login";
 import Signup from "./components/SignUp";
+import ProtectedRoute from "./components/ProtectedRoute"; 
 
 const router = createBrowserRouter([
   {
@@ -18,7 +19,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
+    element: (
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
   },
 ]);
 
